803311cf41b864b031ebbd99467b97c00f889a47
Deboffle Maxime <33808798+Effobless2@users.noreply.github.com>
2020-10-18 12:38:04 +0200

    connect to server + diplay on (#3)

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,31 @@
 import React from "react";
+import ListItem from "../../components/list-item";
 import logo from '../../logo.svg';
+import { ControllerService } from "../../services/ControllerService";
 import './Home.css';
 
-export default class Home extends React.Component {
+interface HomeState {
+    elems: number[];
+  }
+export default class Home extends React.Component<{}, HomeState> {
+    constructor(props: any) {
+        super(props)
+        this.state = {
+            elems: []
+        };
+    }
+
+    componentWillMount() {
+        const service = new ControllerService();
+        service.get().then((result: number[]) => this.setState({elems: result}));
+    }
+
     render() {
         return (
             <div>
+                {this.state.elems.map((value, index) => {
+                    return <ListItem key={index} content={value} />
+                })}
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
                     Edit <code>src/App.tsx</code> and save to reload.
